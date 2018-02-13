7eeb89d8fab8303b988f9aa6ef2c9ac2a11fb26e
= <=>
2018-02-13 11:16:33 +0100

    Change TraceManager functions and corresponding tests

--- collect_data/model.py
+++ collect_data/model.py
@@ -4,35 +4,20 @@ import glob
 from datetime import timedelta
 
 
-def build_condition(bool_parameter, trace1, trace2):
-    """
-    Build conditions for if-loop depending on initial_condition, trace.starttime and trace.endtime.
-    :param bool_parameter: bool that imposes the condition
-    :param trace1: obspy.trace
-    :param trace2: obspy.trace
-    :return:
-    """
-
-    t1_start_time = trace1.stats.starttime
-    # t1_end_time = trace1.stats.endtime
-    t2_start_time = trace2.stats.starttime
-    # t2_end_time = trace2.stats.endtime
-
-    if bool_parameter is True:
-        # add 2 seconds to get same hour from XXh 59min 59 sec
-        t1_start_time += timedelta(seconds=2)
-        t2_start_time += timedelta(seconds=2)
-
-    return t1_start_time.hour == t2_start_time.hour
-
-
 class TraceManager:
 
-    def __init__(self, repository_path, file_name_regexp, start_at_59=True):
+    def __init__(self,
+                 repository_path,
+                 file_name_regexp,
+                 start_at_59=True,
+                 sort_by=lambda trace: trace.stats.starttime
+                 ):
+
         self.repository_path = repository_path
         self.file_name_regexp = file_name_regexp
         self.start_at_59 = start_at_59
-        self.sorted_and_merged_traces = self.sort_and_merge_traces()
+        self.sort_by = sort_by
+        self.traces = self.merge_traces()
 
     def sort_traces(self):
         """
@@ -46,16 +31,15 @@ class TraceManager:
             unsorted_traces.append(trace)
 
         # sorting it
-        sorted_traces = sorted(unsorted_traces, key=lambda trace: trace.stats.starttime)
+        sorted_traces = sorted(unsorted_traces, key=self.sort_by)
         return sorted_traces
 
-    def merge_same_hour(self, unmerged_traces, bool_param):
+    def merge_traces(self):
         """
-        :param unmerged_traces: array of obspy.traces sorted by starttime
-        :param bool_param: boolean that fixes the condition of merging
         :return: array of obspy.traces sorted by starttime and merged if some traces are within the same hour
         """
 
+        unmerged_traces = self.sort_traces()
         merged_traces = []
         i = 0
         stacked_traces = unmerged_traces[0]
@@ -63,10 +47,9 @@ class TraceManager:
             current_trace = unmerged_traces[i]
             next_trace = unmerged_traces[i+1]
 
-            condition = build_condition(bool_param, current_trace, next_trace)
+            is_same_hour = self.is_same_hour(current_trace, next_trace)
 
-            if condition:
-                print(condition)
+            if is_same_hour:
                 stacked_traces += next_trace
             else:  # if no more hour in common
                 # save previous step
@@ -80,20 +63,45 @@ class TraceManager:
 
         return merged_traces
 
-    def sort_and_merge_traces(self):
-        return self.merge_same_hour(self.sort_traces(), bool_param=self.start_at_59)
+    def is_same_hour(self, trace1, trace2):
+        """
+        Build assertions for if-loop depending on self.start_at_59, trace.starttime and trace.endtime.
+        If self.start_at_59 is true, then XXh59-59 is considered to be equal to XX+1h00-00
+        For example:
+        09h59-59 == 10h00-00 is True
+        If self.start_at_59 is false, then the condition is only on entire value of hour
+        For example:
+        09h59-59 is considered to be part of 09h
+        10h00-00 is considered to be part of 10h
+        Then 09h59-59 == 10h00-00 is False
+        :param trace1: obspy.trace
+        :param trace2: obspy.trace
+        :return: Assertion on trace1.start_time and trace2.starttime
+        """
+
+        t1_start_time = trace1.stats.starttime
+        # t1_end_time = trace1.stats.endtime
+        t2_start_time = trace2.stats.starttime
+        # t2_end_time = trace2.stats.endtime
+
+        if self.start_at_59 is True:
+            # add 2 seconds to get same hour from XXh 59min 59 sec
+            t1_start_time += timedelta(seconds=2)
+            t2_start_time += timedelta(seconds=2)
+
+        return t1_start_time.hour == t2_start_time.hour
 
     def get_starttimes(self):
         """
         :return: an array of self.sorted_and_merged_traces starttimes
         """
-        return [trace.stats.starttime for trace in self.sorted_and_merged_traces]
+        return [trace.stats.starttime for trace in self.traces]
 
     def get_endtimes(self):
         """
         :return: an array of self.sorted_and_merged_traces endtimes
         """
-        return [trace.stats.endtime for trace in self.sorted_and_merged_traces]
+        return [trace.stats.endtime for trace in self.traces]
 
 
 # Time functions
