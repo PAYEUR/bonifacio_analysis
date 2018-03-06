0500dfc663b6f87d10fae62b558a30941a9a6e21
= <=>
2018-03-06 18:00:55 +0100

    Add WeatherParser

--- meteo_parsing/model.py
+++ meteo_parsing/model.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 import urllib.request
 from datetime import date, timedelta
+from bs4 import BeautifulSoup
 
 month_dict = {1: 'janvier',
               2: 'fevrier',
@@ -23,9 +24,37 @@ year_string = str(date.year)
 month_string = month_dict[date.month]
 
 start_url_string = 'http://www.infoclimat.fr/observations-meteo/archives'
-end_url_string =  'bonifacio-cap-pertusato/07770.html'
+end_url_string = 'bonifacio-cap-pertusato/07770.html'
 
 url = f"{start_url_string}/{day_string}/{month_string}/{year_string}/{end_url_string}"
 
-with urllib.request.urlopen(url) as f:
-    print(f.read(300))
+
+class WeatherParser:
+
+    def __init__(self,
+                 url_or_file_object):
+
+        self.url_or_file_object = url_or_file_object
+        self.soup = BeautifulSoup(url, 'lxml')
+        self.temp_spans = self.get_temp_spans()
+        self.wind_spans = self.get_wind_spans()
+        self.temp_list = [float(elt.text) for elt in self.temp_spans]
+        self.wind_list = [float(elt.text) for elt in self.wind_spans[::2]]
+        self.wind_gust_list = [float(elt.text) for elt in self.wind_spans[1::2]]
+
+    def get_temp_spans(self):
+        sp = self.soup.find_all('span',
+                                attrs={'style': 'font-weight:bold;margin-top:10px;display:inline-block;font-size:16px'}
+                                )
+        return sp
+
+    def get_wind_spans(self):
+        return self.soup.find_all('span', attrs={'style': 'font-weight:bold'})
+
+
+#with urllib.request.urlopen(url) as f:
+# with open('../tests/test.html', 'r') as f:
+#     wp = WeatherParser(f)
+#     print(wp.temp_list)
+#     print(wp.wind_list)
+#     print(wp.wind_gust_list)
--- tests/test_meteo_parsing.py
+++ tests/test_meteo_parsing.py
@@ -1,2 +1,40 @@
 import pytest
+import urllib.request
+import meteo_parsing.model as mp_model
 
+
+@pytest.fixture()
+def url_test():
+    return 'http://www.infoclimat.fr/observations-meteo/archives/3/novembre/2016/bonifacio-cap-pertusato/07770.html'
+
+
+@pytest.fixture()
+def html_file_path_test():
+    return 'tests/test.html'
+
+
+@pytest.fixture()
+def weather_parser(html_file_path_test):
+    with open(html_file_path_test, 'r') as f:
+        return mp_model.WeatherParser(f)
+
+
+def test_temperature(weather_parser):
+    temp_ref = [15.6, 15.6, 15.7, 15.7, 16.3, 16.9, 16.9, 17.2, 17.9, 18.2, 17.9, 18.5, 18.8, 18.8, 18.7,
+                18.5, 18.1, 18.4, 18.2, 18.2, 17.9, 17.8, 18.0, 17.9]
+
+    assert weather_parser.temp_list == temp_ref
+
+
+def test_wind(weather_parser):
+    wind_ref = [22.0, 22.0, 22.0, 22.0, 22.0, 29.0, 32.0, 32.0, 36.0, 40.0, 43.0, 47.0, 50.0, 50.0, 50.0,
+                50.0, 47.0, 50.0, 47.0, 50.0, 47.0, 47.0, 40.0, 36.0]
+
+    assert weather_parser.wind_list == wind_ref
+
+
+def test_wind_gust(weather_parser):
+    wind_gust_ref = [32.4, 28.8, 32.4, 32.4, 43.2, 46.8, 50.4, 50.4, 57.6, 61.2, 64.8, 72.0, 68.4, 75.6,
+                     75.6, 72.0, 72.0, 64.8, 68.4, 68.4, 64.8, 61.2, 57.6, 54.0]
+
+    assert weather_parser.wind_gust_list == wind_gust_ref
