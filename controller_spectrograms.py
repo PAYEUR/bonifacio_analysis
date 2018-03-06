7fd840aaea42e1c350a5ad0b2df453e109fa0821
= <=>
2018-03-06 15:33:10 +0100

    Adding test.html

--- controller_spectrograms.py
+++ controller_spectrograms.py
@@ -1,29 +1,45 @@
 # -*- coding: utf-8 -*-
 from collect_data import model as cd_model
 from process_data import model as pd_model
+import matplotlib
+matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 import matplotlib.colors as colors
 import numpy as np
-from matplotlib.ticker import FormatStrFormatter
+from matplotlib.ticker import FormatStrFormatter, FixedLocator, FixedFormatter
 import time
+from pathlib import Path
+
+
+def root():
+    return Path(__file__).parents[0]
+
 
 # local settings
-mother_repository = "D:/JBP-Preprog-Recherche/Bonifacio_obspy/Bruit-de-fond"
-data_folders = ['Station-falaise']  # 'Station-reference',
+# mother_repository = "D:/JBP-Preprog-Recherche/Bonifacio_obspy/Bruit-de-fond"
+# data_folders = ['Station-falaise']  # 'Station-reference',
 
-# prod settings
+# prod settings on local machine
 # mother_repository = '//SRV51-NETAPP2/Data_RS/Bonifacio/Bonifacio-bdf-definitif'
 
-# data_folders = ['Falaise_Janv2017', 'Ref_janv2017',
-#                 'Falaise_nov2016', 'Ref_nov2016',
-#                 ]
+# prod settings on EraPido
+mother_repository = '/media/Data_RS/Bonifacio/Bonifacio-bdf-definitif'
+
+data_folders = ['Falaise-nov2016', 'Ref_nov2016',
+                'Falaise_dec2016', 'Ref_dec2016',
+                'Falaise_Janv2017', 'Ref_janv2017',
+                'Falaise_fev2017', 'Ref_fev2017',
+                'Falaise_mars2017', 'Ref_mars2017',
+                'Falaise_av2017', 'Ref_av2017',
+                'Falaise_mai2017', 'Ref_mai2017',
+                ]
 
 # common settings
 directions_dict = {'C00': 'Z', 'C01': 'N', 'C02': 'E'}
 
-results_repository = 'D:/JBP-Preprog-Recherche/Bonifacio_obspy/bonifacio_analysis/results'
+results_repository = str(root()/'results')
 
-reference_file_path = 'D:/JBP-Preprog-Recherche/Bonifacio_obspy/bonifacio_analysis/tests/data_test/Falaise_nov2016/2016.11.06-23.59.59.AG.570009.00.C00.SAC'
+reference_file_path = str(root()/'tests/data_test/Falaise_nov2016/2016.11.06-23.59.59.AG.570009.00.C00.SAC')
 
 # --------------------------------------------------------------------------------------
 # Script:
@@ -31,9 +47,9 @@ start_time = time.time()
 
 spectrograms = dict()
 trace_processor = pd_model.TraceProcessor(reference_file_path,
-                                          decimate_factor=5,
+                                          decimate_factor=4,
                                           freqmin=0.5,
-                                          freqmax=15)
+                                          freqmax=20)
 
 print("Saving frequencies file")
 save_file_f = f"{results_repository}/frequencies.txt"
@@ -55,18 +71,17 @@ for directory in data_folders:
 
         print(f"Computing {title} spectrogram...")
 
-        # TODO: change hours_spectro into an iterable
         hours_spectros = []
 
-        # TODO: change trace_manager.traces into an iterable
         for trace in trace_manager.traces:
             trace_processor.filter_trace(trace)
             p_xx = trace_processor.compute_decimated_spectrum(trace)[0]
             hours_spectros.append(p_xx)
 
-        # TODO: check this cautiously
         sp = np.transpose(np.array(hours_spectros))
+        x_list = trace_manager.get_starttimes() + [trace_manager.get_endtimes()[-1]]
         del hours_spectros
+        del trace_manager
 
         # -------------------------------------------------------------------------
         # saving data in files
@@ -89,10 +104,10 @@ for directory in data_folders:
         ax = fig.add_subplot(1, 1, 1)
 
         ax.set_title(title)
-        ax.set_xlabel('Hours')
+        # ax.set_xlabel('Hours')
         ax.set_ylabel('Frequency (Hz)')
 
-        x = np.arange(len(trace_manager.get_starttimes()))
+        x = np.arange(len(x_list))
         y = trace_processor.filtred_and_decimated_ref_freqs
         Z = sp
 
@@ -100,15 +115,25 @@ for directory in data_folders:
                                 cmap='jet',
                                 norm=colors.LogNorm(vmin=1e3, vmax=1e6),  # logarithmic scaling
                                 )
+
+        # x axis:
+        # after https://stackoverflow.com/questions/17129947/how-to-set-ticks-on-fixed-position-matplotlib
+        name_list = [elt.strftime('%a %d %b') for elt in x_list]
+        x_labels_interval = 5*24  # each 5 days
+        ax.xaxis.set_major_locator(FixedLocator(x[::x_labels_interval]))
+        ax.xaxis.set_major_formatter(FixedFormatter(name_list[::x_labels_interval]))
+        plt.xticks(rotation=70)
+
+        # y axis:
         ax.set_yscale('log')
-        # TODO: replace xaxis by trace_manager.get_starttimes()
-        ax.yaxis.set_major_formatter(FormatStrFormatter('%.0f'))
-        ax.yaxis.set_minor_formatter(FormatStrFormatter('%.0f'))
-        plt.ylim(ymin=1, ymax=np.max(y))
+        ax.yaxis.set_major_formatter(FormatStrFormatter('%.1f'))
+        ax.yaxis.set_minor_formatter(FormatStrFormatter('%.1f'))
+        plt.ylim(ymin=0.5, ymax=np.max(y))
 
         cb = plt.colorbar(picture)
         cb.set_label('Arbitrary Units')
 
+        plt.tight_layout()
         plt.savefig(f"{results_repository}/{title}.png", format='png')
 
 
